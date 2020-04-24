6d8d411cb3b47098d212cb6916244ac4e6da6ea1
RamJett <ramjett@ramnic.com>
2020-04-24 23:22:28 +0000

    make the secondary server key a option
    some cleanup

--- contrib/gpg-genkey.sh
+++ contrib/gpg-genkey.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-TMPFILE="$(mktemp)"
-TMPWDIR="$(mktemp -d)"
-export GNUPGHOME="$TMPWDIR"
 
-echo "what is the hostname (this is the inventory name or secondary"
-echo -n "when generating secondary backup key) ? "
-read varhostname
+echo -n "Is this run to generate the secondary server key (y/n) ? "
+read varsecondary
+if [ "$varsecondary" == 'y' ]; then
+	varhostname="secondary"
+elif [ "$varsecondary" == 'n' ]; then
+	echo
+	echo -n "What is the hostname (the inventory name) ? "
+	read varhostname
+else
+	echo "Please answer (y/n). Exiting ..."
+	exit
+fi
 
+echo
+echo "This passphrase will be asked for you to input at the end so that"
+echo "the key can be exported"
 echo
 echo -n "what is you passphrase (warning this will print to screen) ? "
 read varpassphrase
+
+TMPFILE="$(mktemp)"
+TMPWDIR="$(mktemp -d)"
+export GNUPGHOME="$TMPWDIR"
 
 cat > $TMPFILE <<EOF
  %echo Generating a default key
@@ -34,7 +47,8 @@ EXISTS=$?
 if [ $EXISTS -ne 0 ]; then
   gpg --export-secret-keys $FINGERPRINT > gpg_keys/$varhostname.gpg
 else
-  echo "gpg_keys/$varhostname.gpg already exists"
+	echo
+	echo "gpg_keys/$varhostname.gpg already exists"
 fi
 
 # cleanup
@@ -48,9 +62,8 @@ Add these to the inventory host variables
 
 gpg_passphrase=$varpassphrase gpg_keyid=$FINGERPRINT
 
-Note: The inventory name must be $varhostname unless you are creating the secondary backup gpg key
-
 EOF
 else
-	echo "Key not created. Delete the existing and run again"
+	echo "Key not created. Delete the existing and run again."
+	echo
 fi
